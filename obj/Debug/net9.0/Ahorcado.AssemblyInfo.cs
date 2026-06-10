f3e04723edfd7083c66e7eb67f3d82fd4f09bebe
LORENZO CREO KOCHER <50049697@ort>
2026-06-10 09:05:15 -0300

    clase23

--- obj/Debug/net9.0/Ahorcado.AssemblyInfo.cs
+++ obj/Debug/net9.0/Ahorcado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ahorcado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97846a87448bfda1d2f502afa20ae97cc0d8622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72fdb7761aa5eed1dd33f402e7f581777abe840")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ahorcado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ahorcado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿const Letra = document.getElementById("txtLetra");
+const PalabraOculta  = document.getElementById("PalabraOculta");
+const Intentos1    = document.getElementById("Intentos");
+const Mensaje = document.getElementById("Mensaje");
+const Dguiones = document.getElementById("guiones");
+
+let intentos = 10;
+const pOculta = PalabraOculta.value;
+let palabra="";
+console.log(pOculta.length)
+for(let i = 0; i< pOculta.length; i++ )
+{
+    palabra += "-";
+}
+
+Dguiones.innerHTML = palabra;
+Intentos1.innerHTML= ("Tus intentos: " + intentos);
+
+
+function ArriesgarLetra()
+{
+
+ 
+
+}
+
+
+
